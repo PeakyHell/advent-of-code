f6a970b357c6b10c01acb386187c7f5dc2110831
PeakyHell <louis.vanimpe@student.uclouvain.be>
2025-01-14 22:43:08 +0100

    day 2 part 2

---
+++ 2024/day2/day2_p2.py
@@ -0,0 +1,19 @@
+from day2_p1 import get_reports, is_valid
+
+def count_safe_reports_with_tolerance(file_path):
+    reports = get_reports(file_path)
+    count = 0
+
+    for report in reports:
+        if is_valid(report):
+            count += 1
+        else:
+            for i in range(0, len(report)):
+                temp = list(report)
+                del temp[i]
+                if is_valid(temp):
+                    count += 1
+                    break
+    return count
+
+print(count_safe_reports_with_tolerance("./2024/day2/day2_p1.txt"))
